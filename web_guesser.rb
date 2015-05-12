4c04e14829a6421f03e40cbff40d2141f4b4a06f
Lev Kravinsky <kravinskylev@gmail.com>
2015-05-11 22:48:59 -0600

    buggy counter extension commented out

--- web_guesser.rb
+++ web_guesser.rb
@@ -2,18 +2,32 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 NUMBER = rand(100)
+# @@count = 0
+# message = "Pick a number, any number"
 
 def check(guess)
-  if guess == NUMBER
-    "Correct!"
-  elsif (guess - NUMBER) > 5
+
+  # if @@count > 4
+  #     color = "rgb(150,150,255)"
+  #     @@count = 0
+  #     message = "Ya done goofed. Try again."
+  # end
+
+  if (guess - NUMBER) > 5
+    @@count += 1
     "Way too high!"
   elsif (NUMBER - guess) > 5
+    @@count += 1
     "Way too low!"
   elsif guess > NUMBER
+    @@count += 1
     "Too high!"
   elsif NUMBER > guess
+    @@count += 1
     "Too low!"
+  elsif guess == NUMBER
+    @@count = 0
+    "Correct!"
   end
 end
 
